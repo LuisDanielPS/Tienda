d0a395ac886606f2ebc74a9bc6a266c40354c9ec
LuisDanielPS <lperez40337@ufide.ac.cr>
2023-03-02 21:20:14 -0600

    Semana 7

--- src/main/java/com/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -25,7 +26,7 @@ public class PersonaController {
     @GetMapping("/persona")
     public String index(Model model){
         List<Persona> listaPersona = personaService.getAllPersona();
-        model.addAttribute("titulo","Tabla Personas");
+        model.addAttribute("titulo","Tabla Eventos");
         model.addAttribute("personas",listaPersona);
         return "personas";
     }
@@ -38,11 +39,26 @@ public class PersonaController {
         return "crear";
     }
     
+        @GetMapping("/delete/{id}")
+    public String eliminarPersona(@PathVariable("id")Long idPersona){
+        personaService.delete(idPersona);
+        return "redirect:/persona";
+    }
+    
     @PostMapping("/save")
     public String guardarPersona(@ModelAttribute Persona persona){
         personaService.savePersona(persona);
-        return "redifrect:/persona";
+        return "redirect:/persona";
     }
     
+    @GetMapping("/editPersona/{id}")
+    public String editarPersona(@PathVariable("id")Long idPersona, Model model){
+        Persona persona = personaService.getPersonaById(idPersona);
+        List<Pais> listaPaises= paisService.listCountry();
+        model.addAttribute("persona",persona);
+        model.addAttribute("paises",listaPaises);
+        return "crear";
+    }
+   
     
 }
